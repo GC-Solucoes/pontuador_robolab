ff3be86c9b83b6821771588b24c8138e4cd5c6ab
Gabriel Coimbra De Oliveira Da Silva <54251679+coimbrox@users.noreply.github.com>
2023-10-09 12:02:37 -0300

    Ajustando Pontuador DropDown

--- lib/core/reusable/dropdown.dart
+++ lib/core/reusable/dropdown.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class DropdownReusable extends StatefulWidget {
-  const DropdownReusable({super.key});
+  const DropdownReusable({Key? key}) : super(key: key);
 
   @override
   State<DropdownReusable> createState() => _DropdownReusableState();
@@ -23,20 +23,61 @@ const List<String> list = <String>[
 
 class _DropdownReusableState extends State<DropdownReusable> {
   String dropdownValue = list.first;
+  double pontuacaoSelecionada = 0.0;
+
+  double mapearValorParaPontuacao(String valorSelecionado) {
+    switch (valorSelecionado) {
+      case '0':
+        return 0.0;
+      case '1':
+        return 1.0;
+      case '2':
+        return 2.0;
+      case '3':
+        return 3.0;
+      case '4':
+        return 4.0;
+      case '5':
+        return 5.0;
+      case '6':
+        return 6.0;
+      case '7':
+        return 7.0;
+      case '8':
+        return 8.0;
+      case '9':
+        return 9.0;
+      case '10':
+        return 10.0;
+      default:
+        return 0.0; // Valor padrão
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DropdownMenu<String>(
-      initialSelection: list.first,
-      onSelected: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
-        return DropdownMenuEntry<String>(value: value, label: value);
-      }).toList(),
+    return Column(
+      children: [
+        DropdownButton<String>(
+          value: dropdownValue,
+          onChanged: (String? value) {
+            setState(() {
+              dropdownValue = value!;
+              pontuacaoSelecionada = mapearValorParaPontuacao(value);
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+        Text(
+          'Pontos: $pontuacaoSelecionada',
+          style: const TextStyle(fontSize: 18),
+        ),
+      ],
     );
   }
 }
