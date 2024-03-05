dca206b6dea9f8ac6fd7ec0775f82e7e9ada81cc
Leonardo Franco Lima <leonardo.franco.lima53@gmail.com>
2024-03-05 14:43:30 -0300

    adicionando-imagem

--- lib/tela_login.dart
+++ lib/tela_login.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:pontuador_robolab/features/inicio_pontuador/presentation/pages/home_page.dart';
-import 'database_helper.dart';
+
 
 class TelaLogin extends StatefulWidget {
   const TelaLogin({super.key});
@@ -13,55 +12,6 @@ class TelaLogin extends StatefulWidget {
 }
 
 class _TelaLoginState extends State<TelaLogin> {
-  final TextEditingController _usuarioController = TextEditingController();
-  final TextEditingController _senhaController = TextEditingController();
-  final DatabaseHelper _databaseHelper = DatabaseHelper();
-  final String _rememberLoginKey = 'remember_login';
-  final FocusNode _usuarioFocus = FocusNode();
-  final FocusNode _senhaFocus = FocusNode();
-  bool _rememberLogin = false;
-  bool _isEditing = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadRememberedLogin();
-    _usuarioFocus.addListener((_handleFocusChange));
-    _senhaFocus.addListener((_handleFocusChange));
-  }
-
-  void _handleFocusChange() {
-    setState(() {
-      _isEditing = _usuarioFocus.hasFocus || _senhaFocus.hasFocus;
-    });
-  }
-
-  _loadRememberedLogin() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool rememberLogin = prefs.getBool(_rememberLoginKey) ?? false;
-
-    setState(() {
-      _rememberLogin = rememberLogin;
-      if (_rememberLogin) {
-        String savedUsername = prefs.getString('saved_username') ?? '';
-        String savedPassword = prefs.getString('saved_password') ?? '';
-
-        _usuarioController.text = savedUsername;
-        _senhaController.text = savedPassword;
-      }
-    });
-  }
-
-  _saveRememberedLogin() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(_rememberLoginKey, _rememberLogin);
-
-    if (_rememberLogin) {
-      prefs.setString('saved_username', _usuarioController.text);
-      prefs.setString('saved_password', _senhaController.text);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,83 +25,20 @@ class _TelaLoginState extends State<TelaLogin> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.network(
-                  'https://github.com/coimbrox/pontuador_robolab/raw/0e074b208af47e2f635028f09c15b9692855d5db/lib/assets/image/icon-android.png',
-                  height: 180.0,
-                  width: 180.0,
+                  'https://github.com/GC-Solucoes/pontuador_robolab/blob/main/lib/assets/image/2023-12-20-Logo.png',
+                  height: 350.0,
+                  width: 350.0,
                 ),
-                const SizedBox(height: 16.0),
-                Container(
-                  padding: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: TextField(
-                    controller: _usuarioController,
-                    decoration: const InputDecoration(
-                      labelText: 'Usuário',
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.symmetric(vertical: 7.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Container(
-                  padding: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: TextField(
-                    controller: _senhaController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Senha',
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.symmetric(vertical: 7.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 15.0),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _rememberLogin,
-                      onChanged: (value) {
-                        setState(() {
-                          _rememberLogin = value ?? false;
-                        });
-                      },
-                    ),
-                    const Text('Lembrar login'),
-                  ],
-                ),
-                const SizedBox(height: 30.0),
+                const SizedBox(height: 80.0),
                 SizedBox(
                   width: 220,
                   height: 45,
                   child: ElevatedButton(
                     onPressed: () async {
-                      final usuarios = await _databaseHelper.getUsuarios();
-                      final usuarioEncontrado = usuarios.any(
-                        (user) =>
-                            user['nomeUsuario'] == _usuarioController.text &&
-                            user['senha'] == _senhaController.text,
-                      );
-
-                      if (usuarioEncontrado) {
-                        _saveRememberedLogin();
-                        Navigator.push(
+                      Navigator.push(
                           context,
                           MaterialPageRoute(builder: (context) => const MyHomePage()),
                         );
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Credenciais inválidas'),
-                          ),
-                        );
-                      }
                     },
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
@@ -161,26 +48,13 @@ class _TelaLoginState extends State<TelaLogin> {
                       ),
                     ),
                     child: const Text(
-                      'Login',
+                      'Entrar',
                       style: TextStyle(fontSize: 18.0, color: Colors.black),
                     ),
                   ),
                 ),
                 const SizedBox(height: 40.0),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushReplacementNamed(context, '/cadastro');
-                  },
-                  child: const Text(
-                    'Não tenho cadastro',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.red,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 100.0),
+                const SizedBox(height: 200.0),
                 const Text(
                   'Pontuador RoboLab Caxias Versão 1.2 Dezembro de 2023 Desenvolvido por Gabriel Coimbra de Oliveira da Silva e Leonardo Franco Lima sob orientação dos professores Greice da Silva Lorenzzetti Andreis e André Augusto Andreis IFRS - Campus Caxias do Sul',
                   style: TextStyle(
